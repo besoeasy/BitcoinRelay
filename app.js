6abac095e17d96328816be60f2c7c2738b335683
Aman Singh Katal <besoeasy@users.noreply.github.com>
2024-12-28 19:59:47 +0530

    xc

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const { paintImg } = require("./create/canva.js");
 
 const { commitMsg } = require("./modules/nostr.js");
 
-console.log("IMGBB_API_KEY:", process.env.IMGBB_API_KEY); // Add this line to debug
-
 async function main() {
   const btcprice = await getBitcoinPrice();
 
