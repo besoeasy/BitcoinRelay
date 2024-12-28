05cb260519cbfa6e6243f2c6208c75943c890792
Aman Singh Katal <besoeasy@users.noreply.github.com>
2024-12-28 20:32:33 +0530

    Update nostr.js

--- modules/nostr.js
+++ modules/nostr.js
@@ -44,7 +44,7 @@ async function commitMsg(nsec, content) {
     };
 
     // Compute PoW
-    const difficulty = 12; // Set PoW difficulty
+    const difficulty = 20; // Set PoW difficulty
     const { id, nonce } = computePow(eventTemplate, difficulty);
 
     // Update the event template with PoW tags
