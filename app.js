e4e6125dcc6cd5b41c1e118fcf5135c8c91d1a64
besoeasy <besoeasy@users.noreply.github.com>
2024-12-29 02:10:45 +0530

    Refactor main function to streamline commit message logic and improve readability

--- app.js
+++ app.js
@@ -1,68 +1,43 @@
 require("dotenv").config();
 
-const { uploadToImgbb } = require("./modules/pag.js");
-
+const { uploadToImgbb, getBitcoinPrice } = require("./modules/pag.js");
 const { paintImg } = require("./create/canva.js");
-
 const { commitMsg } = require("./modules/nostr.js");
-
-const { getBitcoinPrice } = require("./modules/pag.js");
-
 const { fetchAllFeeds } = require("./modules/news.js");
 
 async function text2img(msg) {
   const buffer = await paintImg(msg);
-
-  const msgurl = await uploadToImgbb(process.env.IMGBB_API_KEY, buffer);
-
-  return msgurl || null;
+  return uploadToImgbb(process.env.IMGBB_API_KEY, buffer) || null;
 }
 
 async function main() {
-  const random = parseInt(Math.floor(Math.random() * 10));
+  const random = Math.floor(Math.random() * 10);
+  const btcprice = await getBitcoinPrice();
 
-  if (random < 2) {
-    const posts = await fetchAllFeeds();
-
-    const post = posts[Math.floor(Math.random() * posts.length)];
-
-    await commitMsg(
-      process.env.NSEC,
-      `${post.title}  #bitcoin #news        ${post.link}`
-    );
-
-    return;
-  }
-
-  if (random < 6) {
-    const btcprice = await getBitcoinPrice();
-
-    if (btcprice > 1) {
-      await commitMsg(
-        process.env.NSEC,
-        `Bitcoin Is ${btcprice} USD  #bitcoin #crypto #trade`
-      );
+  switch (true) {
+    case random < 2: {
+      const posts = await fetchAllFeeds();
+      const post = posts[Math.floor(Math.random() * posts.length)];
+      await commitMsg(process.env.NSEC, `${post.title} #bitcoin #news ${post.link}`);
+      break;
     }
 
-    return;
-  }
-
-  if (random < 10) {
-    const btcprice = await getBitcoinPrice();
-
-    const msgurl = await text2img(`Bitcoin : ${btcprice} USD`);
-
-    if (msgurl) {
-      await commitMsg(
-        process.env.NSEC,
-        `Bitcoin : ${btcprice} USD   #bitcoin #crypto #trade ${msgurl}`
-      );
+    case random < 6 && btcprice > 1: {
+      await commitMsg(process.env.NSEC, `Bitcoin is ${btcprice} USD #bitcoin #crypto #trade`);
+      break;
     }
 
-    return;
-  }
+    case random < 10: {
+      const msgurl = await text2img(`Bitcoin: ${btcprice} USD`);
+      if (msgurl) {
+        await commitMsg(process.env.NSEC, `Bitcoin: ${btcprice} USD #bitcoin #crypto #trade ${msgurl}`);
+      }
+      break;
+    }
 
-  process.exit(0);
+    default:
+      break;
+  }
 }
 
 main();
