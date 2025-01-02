b0e8f1c701ef3fc63f9242b1ac2ec4e811ca85bc
Aman Singh Katal <besoeasy@users.noreply.github.com>
2025-01-02 23:53:07 +0530

    :P

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ const {
 } = require("./modules/pag.js");
 
 const { plotData, getBTCData, paintPrice } = require("./modules/chaw.js");
-const { commitMsg } = require("./modules/nostr.js");
 const { fetchAllFeeds } = require("./modules/news.js");
+const { commitMsg } = require("./modules/nostr.js");
+
+async function pushIt(text) {
+  await commitMsg(process.env.NSEC, text);
+}
 
 async function imgPrice(msg) {
   const buffer = await paintPrice(msg);
@@ -30,8 +34,7 @@ async function handleBitcoinPriceChart() {
   const msgurl = await imgChart();
 
   if (msgurl) {
-    await commitMsg(
-      process.env.NSEC,
+    await pushIt(
       `Bitcoin Price Action :\n\n` +
         `Min: ${minPrice} USD\n` +
         `Max: ${maxPrice} USD\n` +
@@ -47,13 +50,8 @@ async function handleNewsPost() {
 
   console.log(post);
 
-  await commitMsg(
-    process.env.NSEC,
-    `${post.title} ✍️ ${post.contentSnippet} 
-
-🔗 Read : ${post.link}  
-   
-     #bitcoin #news`
+  await pushIt(
+    `${post.title} ✍️ ${post.contentSnippet} \n\n🔗 Read : ${post.link} \n #bitcoin #news`
   );
 }
 
@@ -62,8 +60,7 @@ async function handleBitcoinPricePost() {
 
   if (Math.random() > 0.7) {
     const sattousd = parseFloat(btcprice / 100000000).toFixed(6);
-    await commitMsg(
-      process.env.NSEC,
+    await pushIt(
       `1 Bitcoin = ${btcprice} USD\n` +
         `1 Satoshi = ${sattousd} USD\n` +
         `\n\n#bitcoin #crypto #trade`
@@ -72,25 +69,21 @@ async function handleBitcoinPricePost() {
     const msgurl = await imgPrice(`${btcprice}`);
 
     if (msgurl) {
-      await commitMsg(
-        process.env.NSEC,
-        `Bitcoin: ${btcprice} USD` + `\n\n #bitcoin #crypto` + `\n ${msgurl}`
+      await pushIt(
+        `Bitcoin: ${btcprice} USD\n\n#bitcoin #crypto\n${msgurl}`
       );
     }
   }
 }
 
 async function handleBiggestTransactionPost() {
-  let biggestTx = null;
+  const biggestTx =
+    Math.random() > 0.5
+      ? await getTransactionWithMaxOutputs()
+      : await getBiggestTransactionDetails();
 
-  if (Math.random() > 0.5) {
-    biggestTx = await getTransactionWithMaxOutputs();
-  } else {
-    biggestTx = await getBiggestTransactionDetails();
-  }
-
-  if (!biggestTx) {
-    await commitMsg(process.env.NSEC, `${biggestTx} #bitcoin #crypto #wallet`);
+  if (biggestTx) {
+    await pushIt(`${biggestTx} #bitcoin #crypto #wallet`);
   }
 }
 
@@ -98,8 +91,7 @@ async function handleLightningNetworkPost() {
   const { node_count, channel_count, avg_capacity, total_capacity } =
     await btcLightning();
 
-  await commitMsg(
-    process.env.NSEC,
+  await pushIt(
     `Bitcoin Lightning Network:\n\n` +
       `${node_count} Nodes\n` +
       `${channel_count} Channels\n` +
@@ -112,34 +104,23 @@ async function handleLightningNetworkPost() {
 async function handleBitcoinFeesPost() {
   const { fee } = await getBitcoinFees();
 
-  await commitMsg(
-    process.env.NSEC,
-    `Current Bitcoin Fee: ${fee} sat/vB \n\n#bitcoin #fees`
-  );
+  await pushIt(`Current Bitcoin Fee: ${fee} sat/vB \n\n#bitcoin #fees`);
 }
 
 async function main() {
-  const random = Math.floor(Math.random() * 6);
+  const tasks = [
+    handleNewsPost,
+    handleBitcoinPriceChart,
+    handleBitcoinPricePost,
+    handleBiggestTransactionPost,
+    handleLightningNetworkPost,
+    handleBitcoinFeesPost,
+  ];
+
+  const randomTask = tasks[Math.floor(Math.random() * tasks.length)];
 
   try {
-    if (random === 0) {
-      await handleNewsPost();
-    }
-    if (random === 1) {
-      await handleBitcoinPriceChart();
-    }
-    if (random === 2) {
-      await handleBitcoinPricePost();
-    }
-    if (random === 3) {
-      await handleBiggestTransactionPost();
-    }
-    if (random === 4) {
-      await handleLightningNetworkPost();
-    }
-    if (random === 5) {
-      await handleBitcoinFeesPost();
-    }
+    await randomTask();
   } catch (error) {
     console.error("An error occurred:", error);
   } finally {
