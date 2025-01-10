1fbb6a318b0ce0555cb1cdf2fab88c21e6edcdeb
besoeasy <besoeasy@users.noreply.github.com>
2025-01-10 09:24:38 +0530

    Adjust random threshold for handleNewsPost execution from 0.1 to 0.5

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ async function main() {
   const randomTask = tasks[Math.floor(Math.random() * tasks.length)];
 
   try {
-    if (Math.random() > 0.1) {
+    if (Math.random() > 0.5) {
       await handleNewsPost();
     }
 
