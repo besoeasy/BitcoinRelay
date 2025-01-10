927aa4ec377288cf3b6789f97b6023c19f1007d6
besoeasy <besoeasy@users.noreply.github.com>
2025-01-10 09:19:02 +0530

    Enhance news feed fetching by introducing random keywords for dynamic queries

--- modules/news.js
+++ modules/news.js
@@ -37,15 +37,20 @@ async function shorturl(url) {
 }
 
 async function fetchAllFeeds() {
+  const newskeywords = ["bitcoin", "crypto", "blockchain"];
+
+  const randomnewskeywords =
+    newskeywords[Math.floor(Math.random() * newskeywords.length)];
+
   const allPosts = await fetchFeed(
-    "https://www.bing.com/news/search?q=bitcoin&format=rss"
+    `https://www.bing.com/news/search?q=${randomnewskeywords}&format=rss`
   );
 
   allPosts.forEach((post) => {
     post.title = post.title.replace(/[^a-zA-Z0-9 ]/g, "");
   });
 
-  let posttosend = allPosts[Math.floor(Math.random() * allPosts.length)];
+  let posttosend = allPosts[Math.floor(Math.random() * 10)];
 
   posttosend.url = await shorturl(posttosend.link);
 
