ca5971fda84126fcafbc60e7e52f89e4697943d6
besoeasy <besoeasy@users.noreply.github.com>
2025-01-14 14:59:05 +0530

    Reduce default expiration time in commitMsg function from 180 to 150 seconds

--- utls/nostr.js
+++ utls/nostr.js
@@ -55,7 +55,7 @@ function calculatePow(event, difficulty) {
   return event;
 }
 
-async function commitMsg(content, nsec, expireAfter = 180, powDifficulty = 4) {
+async function commitMsg(content, nsec, expireAfter = 150, powDifficulty = 4) {
   try {
     const { type, data } = nip19.decode(nsec);
 
