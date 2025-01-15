a20fb5a7ef9dfdb8d28c4b55812ad013791c7af5
besoeasy <besoeasy@users.noreply.github.com>
2025-01-16 01:40:09 +0530

    Update start script to run tests before launching the app and enhance test output

--- test.js
+++ test.js
@@ -1,9 +1,13 @@
 const { hndl_reddit } = require("./modules/reddit.js");
 
 async function main() {
+  console.log("Starting...");
+
   const x = await hndl_reddit();
 
   console.log(x);
+
+  console.log("Done.");
 }
 
 main();
