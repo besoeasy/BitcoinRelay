e5c252f450b3159fb5c2ffdfb681c87402aec1cb
besoeasy <besoeasy@users.noreply.github.com>
2025-01-16 01:55:12 +0530

    Update Reddit API URLs to use the official API endpoint for fetching posts

--- modules/reddit.js
+++ modules/reddit.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const redditurls = ["https://www.reddit.com/r/Bitcoin/hot/.json", "https://www.reddit.com/r/Bitcoin/top/.json", "https://www.reddit.com/r/Bitcoin/new/.json"];
+const redditurls = ["https://api.reddit.com/r/Bitcoin/hot/.json", "https://api.reddit.com/r/Bitcoin/top/.json", "https://api.reddit.com/r/Bitcoin/new/.json"];
 
 const getRandomRedditPost = async () => {
   try {
@@ -10,14 +10,13 @@ const getRandomRedditPost = async () => {
 
     const randomPost = posts[Math.floor(Math.random() * posts.length)];
 
-    const postUrl = `https://www.reddit.com${randomPost.data.permalink}`;
-
-    const detailedPostResponse = await axios.get(`${postUrl}.json`);
+    const detailedPostResponse = await axios.get(`https://api.reddit.com${randomPost.data.permalink}.json`);
     const detailedPost = detailedPostResponse.data[0].data.children[0].data;
 
     const title = detailedPost.title;
     const description = detailedPost.selftext || null;
-    const link = postUrl;
+    
+    const link = `https://www.reddit.com${randomPost.data.permalink}`;
     const images = detailedPost.url_overridden_by_dest ? [detailedPost.url_overridden_by_dest] : [];
 
     return {
