531a4c118eee821363ae1f33f595b2b311d3d95e
besoeasy <besoeasy@users.noreply.github.com>
2025-01-29 10:53:24 +0530

    Update aigen function prompt to include handling for images and links

--- ai/index.js
+++ ai/index.js
@@ -3,7 +3,7 @@ const { HfInference } = require("@huggingface/inference");
 const client = new HfInference(process.env.HFI);
 
 async function aigen(inputx) {
-  const promt =  inputx + "/n/n you are nostr bot, understand the content above, rewrite it in a more professional way and also add a conclusion to it along with a random fact, add related tags, use \n for new line, keep all the content in plain text format, do not use any formatting.";
+  const promt =  inputx + "/n/n you are nostr bot, understand the content above, rewrite it in a more professional way and also add a random fact, add related tags, use \n for new line, keep all the content in plain text format, do not use any formatting. if der is any image or link, add the links to bottom";
 
   const chatCompletion = await client.chatCompletion({
     model: "deepseek-ai/DeepSeek-V3",
