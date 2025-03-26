7d5076c80b2388d63daa81bce53316c604f121c0
besoeasy <besoeasy@users.noreply.github.com>
2025-03-26 20:35:15 +0530

    Refactor app.js to improve content handling and logging for Nostr posts

--- app.js
+++ app.js
@@ -30,18 +30,23 @@ async function main() {
   ]);
 
   try {
-    const content2 = await handler_data[0]();
+    const content = await handler_data[0]();
 
-    console.log("\n\n\n" + content2 + "\n\n\n");
+    const content2 = await handler_data[1]();
 
-    console.log("Pushing to Nostr...");
+    console.log(content + "\n\n\n" + content2);
 
-    const postResult = await postToNostr(process.env.NSEC, content2, {
+    const postResult = await postToNostr(process.env.NSEC, content, {
       expirationDays: 20,
     });
 
     console.log("Pushed to Nostr:", postResult.eventId);
-    
+
+    const postResult2 = await postToNostr(process.env.NSEC, content2, {
+      expirationDays: 25,
+    });
+
+    console.log("Pushed to Nostr:", postResult2.eventId);
   } catch (error) {
     console.error("Error in execution:", error);
   } finally {
